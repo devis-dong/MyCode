d727fafc7490bb401338fa36382e951652f23544
devis-dong <devis.dong>
2021-05-16 07:27:12 +0800

    add cpp and python files and somesettings to use vscode

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,18 @@
-print("hello python.")
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pixel_transform as pt
+
+
+if __name__ == '__main__':
+    print("current working space:", os.getcwd())
+
+    img = mpimg.imread('../Data/imgs/0.jpeg')
+    plt.figure('image_origin')
+    plt.imshow(img)
+
+    img = pt.multiply_and_add_with_a_constant(img, 2, 9)
+    plt.figure('image_transform')
+    plt.imshow(img)
+    plt.show()
---
+++ Python/pixel_transform.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def multiply_and_add_with_a_constant(img, a, b):
+    return img * a + b
+
+
+    
+
