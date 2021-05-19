4935adf60198dc5e83b43bd3315c44381aa7ef3c
devis-dong <devis.dong@gmail.com>
2021-05-19 21:11:54 +0800

    add nonlinear filters

--- Python/LinearFilter.py
+++ Python/LinearFilter.py
@@ -3,37 +3,39 @@ import itertools
 
 from numpy.core.fromnumeric import ndim, shape
 from numpy.lib import RankWarning, pad
+from numpy.lib.index_tricks import AxisConcatenator
+from numpy.lib.shape_base import expand_dims
+
+from RandomizedSelection import randomizedPartition
 
 vec_add = lambda a,b : tuple([x+y for x,y in zip(a,b)])
 
-def convolve(img:np.ndarray, kernel:np.ndarray, step=1, pad_size=0):
+def filter(img:np.ndarray, kernel:np.ndarray, step=1, pad_size=0):
     if 2 == kernel.ndim:
-        return convolve_2d(img,kernel, step, pad_size)
+        return filter_2d(img,kernel, step, pad_size)
     img_pad = np.pad(img, [(pad_size,)]*kernel.ndim + [(0,)]*(img.ndim-kernel.ndim))
-    k_idx = iterIdx(kernel.shape)
     o_idx = iterIdx(tuple([img_pad.shape[i]-kernel.shape[i]+1 for i in range(kernel.ndim)]), step)
-    shape_out = [int((2*pad_size+w-kw)/step + 1) for w, kw in zip(img.shape, kernel.shape)]
+    shape_out = tuple([int((2*pad_size+w-kw)/step + 1) for w, kw in zip(img.shape, kernel.shape)])
+    shape_rem = tuple([img.shape[k] for k in range(kernel.ndim, img.ndim)])
     i_idx = iterIdx(shape_out)
-    for k in range(kernel.ndim, img.ndim):
-        shape_out.append(img.shape[k])
-    img_out = np.zeros(tuple(shape_out))
+    shape_out += shape_rem
+    img_out = np.zeros(shape_out)
+    ker = np.expand_dims(kernel, axis=tuple(range(kernel.ndim, img_pad.ndim))) if kernel.ndim < img_pad.ndim else kernel
     for i, o in zip(i_idx, o_idx):
-        for k in k_idx:
-            img_out[i] += img_pad[vec_add(o, k)]*kernel[k]
-            # print(img_pad[vec_add(o, i)]*kernel[i])
+        img_out[i] = np.sum((img_pad[tuple([range(x, x+w) for x, w in zip(o, kernel.shape)])]*ker).reshape((-1,) + shape_rem), axis=0)
     return img_out
 
-def convolve_2d(img:np.ndarray, kernel:np.ndarray, step=1, pad_size=0):
+def filter_2d(img:np.ndarray, kernel:np.ndarray, step=1, pad_size=0):
     img_pad = np.pad(img, [(pad_size,)]*kernel.ndim + [(0,)]*(img.ndim-kernel.ndim))
-    shape_out = [int((2*pad_size+w-kw)/step + 1) for w, kw in zip(img.shape, kernel.shape)]
-    for k in range(kernel.ndim, img.ndim):
-        shape_out.append(img.shape[k])
-    img_out = np.zeros(tuple(shape_out))
+    shape_out = tuple([int((2*pad_size+w-kw)/step + 1) for w, kw in zip(img.shape, kernel.shape)])
+    shape_rem = tuple([img.shape[k] for k in range(kernel.ndim, img.ndim)])
+    shape_out += shape_rem
+    img_out = np.zeros(shape_out)
+    ker = np.expand_dims(kernel, axis=tuple(range(kernel.ndim, img_pad.ndim))) if kernel.ndim < img_pad.ndim else kernel
     for i, y0 in enumerate(range(0, img_pad.shape[0]-kernel.shape[0]+1, step)):
         for j, x0 in enumerate(range(0, img_pad.shape[1]-kernel.shape[1]+1, step)):
-            for y in range(kernel.shape[0]):
-                for x in range(kernel.shape[1]):
-                    img_out[i, j] += img_pad[y0+y, x0+x] * kernel[y, x]
+            img_out[i, j] = np.sum((img_pad[y0:y0+kernel.shape[0], x0:x0+kernel.shape[1]] * ker).reshape((-1,) + shape_rem), axis=0)
+            # img_out[i, j] = np.sum((img_pad[tuple([range(x, x+w) for x, w in zip((y0, x0), kernel.shape)])] * ker).reshape((-1,) + shape_rem), axis=0)
     return img_out
 
 # def padImg(img:np.ndarray, pad_size=None):
@@ -44,7 +46,7 @@ def convolve_2d(img:np.ndarray, kernel:np.ndarray, step=1, pad_size=0):
 #     return img_pad
 
 def iterIdx(shape, step=1):
-    return list(itertools.product(*[range(0, i, step) for i in shape]))
+    return itertools.product(*[range(0, i, step) for i in shape])
 
 def generateGaussianKernel(ksize=3, sigma=0.8):
     kernel = np.zeros((ksize, ksize))
@@ -62,7 +64,7 @@ def generateLaplacianKernel():
 
 def gaussianFilter(img:np.ndarray, ksize=3, sigma=0.8):
     kernel = generateGaussianKernel(ksize, sigma)
-    return convolve(img, kernel, step=1, pad_size=1)
+    return filter(img, kernel, step=1, pad_size=1)
 
 def generateGaussianVector(ksize=3, sigma=0.8):
     vec = np.zeros((ksize))
@@ -76,13 +78,13 @@ def generateGaussianVector(ksize=3, sigma=0.8):
 def fastGaussianFilter(img:np.ndarray, ksize=3, sigma=0.8):
     vec = generateGaussianVector(ksize, sigma)
     img_conv = np.pad(img, [(1,)]*2+[(0,)]*(img.ndim-2))
-    img_conv = convolve(img, vec.reshape(1, -1), step=1, pad_size=0)
-    img_conv = convolve(img, vec.reshape(-1, 1), step=1, pad_size=0)
+    img_conv = filter(img, vec.reshape(1, -1), step=1, pad_size=0)
+    img_conv = filter(img, vec.reshape(-1, 1), step=1, pad_size=0)
     return img_conv
 
 def laplacianFilter(img:np.ndarray):
     kernel = generateLaplacianKernel()
-    return convolve(img, kernel, step=1, pad_size=1)
+    return filter(img, kernel, step=1, pad_size=1)
 
 def generateLaplacianOfGaussianKernel(ksize=3, sigma=0.8):
     kernel = np.zeros((ksize, ksize))
@@ -96,20 +98,44 @@ def generateLaplacianOfGaussianKernel(ksize=3, sigma=0.8):
 
 def laplacianOfGaussianFilter(img:np.ndarray, ksize=3, sigma=0.8):
     kernel = generateLaplacianOfGaussianKernel(ksize, sigma)
-    return convolve(img, kernel, step=1, pad_size=1)
+    return filter(img, kernel, step=1, pad_size=1)
 
 def fastLaplacianOfGaussianFilter(img:np.ndarray, ksize=3, sigma=0.8):
     kernel = generateLaplacianOfGaussianKernel(ksize, sigma)
-    return convolve(img, kernel, step=1, pad_size=1)
+    return filter(img, kernel, step=1, pad_size=1)
 
 def separableFilter(img:np.ndarray, kernel:np.ndarray, step=1, pad_size=0):
     U, s, VT = np.linalg.svd(kernel)
     u, v = (s[0]**0.5)*U[:, [0]], (s[0]**0.5)*VT[[0], :]
     img_conv = np.pad(img, [(pad_size,)]*kernel.ndim+[(0,)]*(img.ndim-kernel.ndim))
-    img_conv = convolve(img_conv, v, step=step, pad_size=0)
-    img_conv = convolve(img_conv, u, step=step, pad_size=0)
+    img_conv = filter(img_conv, v, step=step, pad_size=0)
+    img_conv = filter(img_conv, u, step=step, pad_size=0)
     return img_conv
 
+def summedAreaTable(img:np.ndarray):
+    s = np.zeros_like(img)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            s[i, j] = (0 if i-1 < 0 else s[i-1, j]) + (0 if j-1 < 0 else s[i, j-1]) - (0 if i-1 < 0 or j-1 < 0 else (s[i-1, j-1])) + img[i, j]
+    return s
+
+areaComputation = lambda a, i0, j0, i1, j1 : a[i1, j1] - (0 if j0-1 < 0 else a[i1, j0-1]) - (0 if i0-1 < 0 else a[i0-1, j1]) + (0 if i0-1 < 0 or j0-1 < 0 else a[i0-1, j0-1])
+
+# def areaComputation(a:np.ndarray, i0, j0, i1, j1):
+#     s = a[i1, j1] - (0 if j0-1 < 0 else a[i1, j0-1]) - (0 if i0-1 < 0 else a[i0-1, j1]) + (0 if i0-1 < 0 or j0-1 < 0 else a[i0-1, j0-1])
+#     return s
+
+def meanFilter(img:np.ndarray, ksize):
+    img_out = np.zeros_like(img)
+    shape_rem = tuple([img.shape[k] for k in range(2, img.ndim)])
+    r = int(ksize/2)
+    # summed_area = summedAreaTable(img)
+    # k = 1 / (2 * r + 1)**2
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            # img_out[i, j] = areaComputation(summed_area, max(i-r, 0), max(j-r, 0), min(i+r, img.shape[0]-1), min(j+r, img.shape[1]-1))
+            img_out[i, j] = np.mean((img[max(i-r, 0):min(i+r+1, img.shape[0]), max(j-r, 0):min(j+r+1, img.shape[1])]).reshape((-1,)+shape_rem), axis=0)
+    return img_out
 
 
     
--- Python/main.py
+++ Python/main.py
@@ -5,18 +5,23 @@ import time
 
 import PointOperators as po
 import LinearFilter as lf
+import RandomizedSelection as rs
+import NonlinearFilter as nlf
 
 
 if __name__ == '__main__':
     print("current working space:", os.getcwd())
     print('running ......')
-    img0 = cv.resize(cv.imread('../Data/imgs/2.jpg'), (512, 512))
+
+    kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]) / 8
+
+    img0 = cv.resize(cv.imread('../Data/imgs/2.jpg'), (256, 256))
     # img1 = cv.resize(cv.imread('../Data/imgs/1.jpg'), (512, 512))
     # img0 = cv.resize(cv.imread('../Data/imgs/girl1.jpg'), (256, 256))
-    # img0 = cv.cvtColor(img0, cv.COLOR_BGR2GRAY)
+    img0 = cv.cvtColor(img0, cv.COLOR_BGR2GRAY)
 
     # time0 = time.time()
-    # cv.imshow('img0', img0)
+    cv.imshow('img0', img0)
     # cv.imshow('image_transform0', pt.gainAndBias(img0, 2, 0).astype(np.uint8))
     # cv.imshow('image_transform1', pt.gainAndBias(img0, np.full(img0.shape, 1), 0).astype(np.uint8))
     # cv.imshow('image_transform2', pt.dyadic(img0, img1, 0.5).astype(np.uint8))
@@ -24,12 +29,15 @@ if __name__ == '__main__':
     # cv.imshow('img0_hist_equalized', po.histogramEqualization(img0).astype(np.uint8))
     # cv.imshow('img0_block_equalized', po.blockHistgramEqualization(img0, (64, 64)).astype(np.uint8))
     # cv.imshow('img0_adp_block_equalized', po.locallyAdaptiveHistogramEqualization(img0, (256, 256)).astype(np.uint8))
-    # kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]) / 8
+    
     # time0 = time.time()
-    # cv.imshow('img0_conv', lf.convolve(img0, kernel, step=1, pad_size=1).astype(np.uint8))
+    # cv.imshow('img0_conv', lf.filter(img0, kernel, step=1, pad_size=1).astype(np.uint8))
     # print('conv:', time.time()-time0)
     # time0 = time.time()
-    # cv.imshow('img0_conv_2d', lf.convolve_2d(img0, kernel, step=1, pad_size=1).astype(np.uint8))
+    # cv.imshow('img0_conv_2d', lf.filter_2d(img0, kernel, step=1, pad_size=1).astype(np.uint8))
+    # print('conv_2d:', time.time()-time0)
+    # time0 = time.time()
+    # cv.imshow('img0_cv_conv_2d', cv.filter2D(img0, -1, kernel))
     # print('conv_2d:', time.time()-time0)
     # time0 = time.time()
     # cv.imshow('img0_gaussian', lf.gaussianFilter(img0, ksize=3, sigma=0.8).astype(np.uint8))
@@ -37,12 +45,33 @@ if __name__ == '__main__':
     # time0 = time.time()
     # cv.imshow('img0_fastGaussian', lf.fastGaussianFilter(img0, ksize=3, sigma=0.8).astype(np.uint8))
     # print('fastGaussian:', time.time()-time0)
-    cv.imshow('img0_laplacian', lf.laplacianFilter(img0).astype(np.uint8))
+    # cv.imshow('img0_laplacian', lf.laplacianFilter(img0).astype(np.uint8))
     # cv.imshow('img0_cv_lalacian', cv.Laplacian(img0, -1))
-    cv.imshow('img0_laplacianOfGaussian', lf.laplacianOfGaussianFilter(img0).astype(np.uint8))
+    # cv.imshow('img0_laplacianOfGaussian', lf.laplacianOfGaussianFilter(img0).astype(np.uint8))
     # time0 = time.time()
     # cv.imshow('img0_separable_filter', lf.separableFilter(img0, kernel, step=1, pad_size=1).astype(np.uint8))
     # print('separable conv:', time.time()-time0)
+
+    # a = np.ones((5, 5, 3))
+    # s = lf.summedAreaTable(a)
+    # print(lf.areaComputation(s, 1, 1, 3, 3))
+
+    # a = np.arange(17)
+    # print(rs.randomizedSelect(a, 0, a.shape[0]-1, 9))
+
+    # time0 = time.time()
+    # cv.imshow('img0_mean_filter', lf.meanFilter(img0, 3).astype(np.uint8))
+    # print('mean filter:', time.time()-time0)
+
+    # time0 = time.time()
+    # cv.imshow('img0_median_filter', nlf.medianFilter(img0, 3).astype(np.uint8))
+    # print('median filter:', time.time()-time0)
+
+    time0 = time.time()
+    cv.imshow('img0_weighted_median', nlf.weightedMedianFilter(img0, np.random.randint(1, 10, size=(img0.shape[0], img0.shape[1])), 3).astype(np.uint8))
+    print('weighted median:', time.time()-time0)
+
+    
 
     cv.waitKey(0)
 
