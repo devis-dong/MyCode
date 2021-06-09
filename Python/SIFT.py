6aba4d7a073e5f9e6f593ad12d2e2808bda256b2
devis-dong <devis.dong@gmail.com>
2021-06-09 20:53:42 +0800

    modify sift

--- Python/SIFT.py
+++ Python/SIFT.py
@@ -1,48 +1,74 @@
+from typing import Tuple
 import numpy as np
 
 SIFT_MAX_INTERP_STEPS = 5
 SIFT_IMG_BORDER = 1
 FEATURE_MAX_D = 128
+SIFT_ORI_HIST_BINS = 36
+SIFT_ORI_PEAK_RATIO = 0.8
 
 class feature:
-    def __init__(self) -> None:
-        self.x = None                           # /**< x coord */
-        self.y = None                           # /**< y coord */
-        self.a = None                           # /**< Oxford-type affine region parameter */
-        self.b = None                           # /**< Oxford-type affine region parameter */
-        self.c = None                           # /**< Oxford-type affine region parameter */
-        self.scl = None                         # /**< scale of a Lowe-style feature */
-        self.ori = None                         # /**< orientation of a Lowe-style feature */
-        self.d = None                           # /**< descriptor length */
-        self.descr = [None]*FEATURE_MAX_D       # /**< descriptor */
-        self.type = None                        # /**< feature type, OXFD or LOWE */
-        self.category = None                    # /**< all-purpose feature category */
-        self.img_pt = None                      # /**< location in image */
-        self.mdl_pt = None                      # /**< location in model */
-        self.feature_data = None                # /**< user-definable data */
+    def __init__(self, x=None, y=None, a=None, b=None, c=None, scl=None, ori=None, d=None, descr=None, type=None, category=None, img_pt=None, mdl_pt=None, ddata=None) -> None:
+        self.x = x                           # /**< x coord */
+        self.y = y                           # /**< y coord */
+        self.a = a                           # /**< Oxford-type affine region parameter */
+        self.b = b                           # /**< Oxford-type affine region parameter */
+        self.c = c                           # /**< Oxford-type affine region parameter */
+        self.scl = scl                         # /**< scale of a Lowe-style feature */
+        self.ori = ori                         # /**< orientation of a Lowe-style feature */
+        self.d = d                           # /**< descriptor length */
+        self.descr = descr       # /**< descriptor */
+        self.type = type                        # /**< feature type, OXFD or LOWE */
+        self.category = category                    # /**< all-purpose feature category */
+        self.img_pt = img_pt                      # /**< location in image */
+        self.mdl_pt = mdl_pt                      # /**< location in model */
+        self.ddata:detection_data = ddata                # /**< user-definable data */
+    def copy(self):
+        return feature(self.x, self.y,self.a, self.b, self.c, self.scl, self.ori, self.d, self.descr, self.type, self.category, self.img_pt, self.mdl_pt, self.ddata)
+
+class detection_data:
+    def __init__(self, r=None, c=None, octv=None, intvl=None, subintvl=None, scl_octv=None) -> None:
+        self.r = r
+        self.c = c
+        self.octv = octv
+        self.intvl = intvl
+        self.subintvl = subintvl
+        self.scl_octv = scl_octv
 
 
-def sift_feature(img:np.ndarray, intvls, sigma, contr_thr, curv_thr, dbl, descr_width, descr_hist_bins):
-    feat = feature()
-    return feat
+def sift_features(img:np.ndarray, intvls, sigma, contr_thr, curv_thr, dbl, descr_width, descr_hist_bins):
+    # 1, generate gaussian pyramid
+    octvs=int(np.log2(min(img.shape[0:2]))-3)
+    gaussian_pyr = buildGaussianPyramid(img, octvs, intvls=4, sigma=1.6)
+    # 2, generate difference of gaussian
+    dog_pyr = buildDiffrenceOfGaussain(gaussian_pyr)
+    # 3, detect extrema
+    features = scaleSpaceExtrema(dog_pyr, octvs, intvls, contr_thr, curv_thr)
+    # 4, calculate orientation
+
+
+    return None
 
 
 def buildGaussianPyramid(img:np.ndarray, octvs, intvls, sigma):
-    gaussian_pyr = [[None]*(intvls+3)]*octvs
+    gaussian_pyr = []
     k = pow(2, 1.0/intvls)
     sig = [0] * (intvls+3)
     sig[0] = sigma
-    sig[1] = sigma * np.sqrt(k*k- 1)
+    sig[1] = sigma * np.sqrt(k*k - 1)
     for i in range(2, len(sig)):
         sig[i] = sig[i-1] * k
     for o in range(octvs):
+        octv_arr = []
         for i in range(intvls+3):
             if 0 == o and 0 == i:
-                gaussian_pyr[o][i] = img
+                octv_arr.append(img)
             elif 0 == i:
-                gaussian_pyr[o][i] = downsample(gaussian_pyr[o-1][intvls])
+                octv_arr.append(downsample(gaussian_pyr[-1][intvls]))
             else:
-                gaussian_pyr[o][i] = gaussianBlur(gaussian_pyr[o][i-1], sig[i])
+                octv_arr.append(gaussianBlur(octv_arr[-1], sig[i]))
+        gaussian_pyr.append(octv_arr)
+                
     return gaussian_pyr
 
 def gaussianBlur(img:np.ndarray, sigma=0.8):
@@ -55,7 +81,7 @@ def generateGaussianKernel(ksize=3, sigma=0.8):
     origin = int(ksize / 2)
     for y in range(ksize):
         for x in range(ksize):
-            kernel[y, x] = np.e**(-((x-origin)**2 + (y-origin)**2)/(2*sigma**2))
+            kernel[y, x] = np.exp(-((x-origin)**2 + (y-origin)**2)/(2*sigma**2))
     kernel /= np.sum(kernel)
     return kernel
 
@@ -89,9 +115,9 @@ def buildDiffrenceOfGaussain(gaussian_pyr):
     return dog_pyr
 
 def scaleSpaceExtrema(dog_pyr, octvs, intvls, contr_thr, curv_thr):
-    extremum_arr = []
+    features = []
     prelim_contr_thr = 0.5 * contr_thr / intvls
-    for o in range(len(dog_pyr)):
+    for o in range(octvs):
         h, w = dog_pyr[o][0].shape[0:2]
         for i in range(1, len(dog_pyr[o]-1)):
             for r in range(SIFT_IMG_BORDER, h-SIFT_IMG_BORDER):
@@ -99,9 +125,9 @@ def scaleSpaceExtrema(dog_pyr, octvs, intvls, contr_thr, curv_thr):
                     if np.abs(dog_pyr[o][i][r, c])>prelim_contr_thr and isExtremum(dog_pyr, o, i, r, c):
                         feat = interpExtremum(dog_pyr, o, i, r, c, intvls, contr_thr)
                         if feat:
-                            if not is_too_edge_like(dog_pyr, feat[0], feat[1], feat[2], feat[3], curv_thr):
-                                extremum_arr.append(feat)
-    return extremum_arr
+                            if not is_too_edge_like(dog_pyr, feat.ddata.octv, feat.ddata.intvl, feat.ddata.r, feat.ddata.c, curv_thr):
+                                features.append(feat)
+    return features
 
 def isExtremum(dog_pyr, o, i, r, c):
     return ((3*r+c == np.argmin(dog_pyr[o][i][r-1:r+2, c-1:c+2])
@@ -115,15 +141,15 @@ def derive3d(dog_pyr, o, i, r, c):
     dx = (dog_pyr[o][i][r, c+1] - dog_pyr[o][i][r, c-1]) / 2
     dy = (dog_pyr[o][i][r+1, c] - dog_pyr[o][i][r-1, c]) / 2
     di = (dog_pyr[o][i+1][r, c] - dog_pyr[o][i-1][r, c]) / 2
-    return np.array([dx, dy, di])
+    return np.array([[dx], [dy], [di]])
 
 def hessian3d(dog_pyr, o, i, r, c):
     dxx = dog_pyr[o][i][r, c+1] + dog_pyr[o][i][r, c-1] - 2*dog_pyr[o][i][r, c]
     dyy = dog_pyr[o][i][r+1, c] + dog_pyr[o][i][r-1, c] - 2*dog_pyr[o][i][r, c]
     daa = dog_pyr[o][i+1][r, c] + dog_pyr[o][i-1][r, c] - 2*dog_pyr[o][i][r, c]
-    dxy = dog_pyr[o][i][r+1, c+1] + dog_pyr[o][i][r-1, c-1] - dog_pyr[o][i][r-1, c+1] - dog_pyr[o][i][r+1, c-1]
-    dxa = dog_pyr[o][i+1][r, c+1] + dog_pyr[o][i-1][r, c-1] - dog_pyr[o][i-1][r, c+1] - dog_pyr[o][i+1][r, c-1]
-    dya = dog_pyr[o][i+1][r+1, c] + dog_pyr[o][i-1][r-1, c] - dog_pyr[o][i-1][r+1, c] - dog_pyr[o][i+1][r-1, c]
+    dxy = (dog_pyr[o][i][r+1, c+1] + dog_pyr[o][i][r-1, c-1] - dog_pyr[o][i][r-1, c+1] - dog_pyr[o][i][r+1, c-1])/4.0
+    dxa = (dog_pyr[o][i+1][r, c+1] + dog_pyr[o][i-1][r, c-1] - dog_pyr[o][i-1][r, c+1] - dog_pyr[o][i+1][r, c-1])/4.0
+    dya = (dog_pyr[o][i+1][r+1, c] + dog_pyr[o][i-1][r-1, c] - dog_pyr[o][i-1][r+1, c] - dog_pyr[o][i+1][r-1, c])/4.0
     return np.array([[dxx, dxy, dxa], [dxy, dyy, dya], [dxa, dya, daa]])
 
 def interpStep(dog_pyr, o, i, r, c):
@@ -136,15 +162,23 @@ def interpValue(dog_pyr, o, i, r, c, offset):
     dD = derive3d(dog_pyr, o, i, r, c)
     return dog_pyr[o][i][r, c] + 0.5*np.dot(dD.T, offset)[0, 0]
 
-def interpExtremum(dog_pyr, o, i, r, c, intvls, contr_thr):
+def interpExtremum(dog_pyr, o, i, r, c, intvls, contr_thr, sigma):
     for i in range(SIFT_MAX_INTERP_STEPS):
         offset = interpStep(dog_pyr, o, i, r, c)
         if (np.abs(offset) < 0.5).all():
             contr = interpValue(dog_pyr, o, i, r, c, offset)
-            if np.abs(contr) < contr_thr:
-                feat = feature()
-                feat
-                return (o, i, r, c, (r+offset[1, 0])*pow(2, o), (c+offset[0, 0])*pow(2, o), offset[2, 0])
+            if np.abs(contr) < contr_thr/intvls:
+                return None
+            feat = feature()
+            feat.ddata.r = r
+            feat.ddata.c = c
+            feat.ddata.octv = o
+            feat.ddata.intvl = i
+            feat.ddata.subintvl = offset[2, 0]
+            feat.ddata.scl_octv = sigma * pow(2, (i+offset[2, 0])/intvls)
+            feat.img_pt = ((c+offset[0, 0])*pow(2, o), (r+offset[1, 0])*pow(2, o))
+            feat.scl = sigma * pow(2.0, o + (i+offset[2, 0])/intvls)
+            return feat
         else:
             i += np.round(offset[2, 0])
             r += np.round(offset[1, 0])
@@ -156,10 +190,12 @@ def interpExtremum(dog_pyr, o, i, r, c, intvls, contr_thr):
 def is_too_edge_like(dog_pyr, o, i, r, c, curv_thr):
     dxx = dog_pyr[o][i][r, c+1] + dog_pyr[o][i][r, c-1] - 2*dog_pyr[o][i][r, c]
     dyy = dog_pyr[o][i][r+1, c] + dog_pyr[o][i][r-1, c] - 2*dog_pyr[o][i][r, c]
-    dxy = dog_pyr[o][i][r+1, c+1] + dog_pyr[o][i][r-1, c-1] - dog_pyr[o][i][r-1, c+1] - dog_pyr[o][i][r+1, c-1]
+    dxy = (dog_pyr[o][i][r+1, c+1] + dog_pyr[o][i][r-1, c-1] - dog_pyr[o][i][r-1, c+1] - dog_pyr[o][i][r+1, c-1])/4.0
     trH = dxx + dyy
     detH = dxx*dyy - dxy*dxy
-    return (trH**2)/detH >= curv_thr
+    if detH <= 0:
+        return True
+    return (trH**2)/detH >= ((curv_thr+1)**2)/curv_thr
 
 def upNearestOdd(a):
     b = round(a)
@@ -167,32 +203,31 @@ def upNearestOdd(a):
         b += 1
     return b
 
-def gradientMatrix(img:np.ndarray, r, c, ksize):
-    kr = int(ksize/2)
-    win = img[r-kr:r+kr+1, c-kr:c+kr+1]
+def gradientMatrix(img:np.ndarray, r, c, rad):
+    win = img[r-rad:r+rad+1, c-rad:c+rad+1]
     dy = np.gradient(win, axis=0)
     dx = np.gradient(win, axis=1)
     grad_mat = np.zeros((win.shape + (2, )))
     grad_mat[:, :, 0] = (dy**2 + dx**2)**0.5
-    grad_mat[:, :, 1] = np.arctan(dy/dx)
+    grad_mat[:, :, 1] = np.arctan(dy/dx)%(2*np.pi)
     return grad_mat
 
-def gradientHistogram(grad_mat, mag_wgt):
-    bins_cnt = 36
-    hist = [0] * bins_cnt
+def gradMat2Hist(grad_mat, mag_wgt, n):
+    hist = [0] * n
     mag_mat = grad_mat[:, :, 0] * mag_wgt
-    ori_mat = (grad_mat[:, :, 1]*18/np.pi).astype(np.uint)
-    h, w = ori_mat.shape[0:2]
+    bin_idx = (grad_mat[:, :, 1]/(2*np.pi)*n).astype(np.uint)
+    h, w = bin_idx.shape[0:2]
     for i in range(h):
         for j in range(w):
-            hist[ori_mat[i, j]] += mag_mat[i, j]
+            hist[bin_idx[i, j]] += mag_mat[i, j]
     return hist
 
-def smoothGradHist(grad_hist):
+def smoothOriHist(grad_hist):
     n = len(grad_hist)
     g_hist = [0]*n
     for i in range(n):
-        g_hist[i] = (grad_hist[(i-2)%n]+grad_hist[(i+2)%n])/16 + (4*(grad_hist[(i-1)%n]+grad_hist[(i+1)%n]))/16 + (6*grad_hist[i])/16
+        # g_hist[i] = (grad_hist[(i-2)%n]+grad_hist[(i+2)%n])/16 + (4*(grad_hist[(i-1)%n]+grad_hist[(i+1)%n]))/16 + (6*grad_hist[i])/16
+        g_hist[i] = 0.25*(grad_hist[(i-1)%n]+grad_hist[(i+1)%n]) + 0.5*grad_hist[i]
     return g_hist
 
 def hist2grad(hist, mag_thr):
@@ -208,7 +243,30 @@ def hist2grad(hist, mag_thr):
     return grad
 
 def interpHistPeak(vl, vi, vr):
-    return (vl - vr) / (2*(vl + vr - 2*vi)), (vr-vl)**2/(8*(2*vi-vl-vr)) + vi
+    return 0.5*(vl - vr) / (vl + vr - 2*vi), 0.125*(vr-vl)**2/(2*vi-vl-vr) + vi
+
+def calculateFeatureOris(features, gaussian_pyr):
+    feats = []
+    feat:feature
+    for feat in features:
+        hist = oriHist(gaussian_pyr[feat.ddata.octv][feat.ddata.intvl],
+                               feat.ddata.r, feat.ddata.c, SIFT_ORI_HIST_BINS,
+                               int(np.round(3*1.5*feat.ddata.scl_octv)),
+                               1.5*feat.ddata.scl_octv)
+        hist = smoothOriHist(hist)
+        grad = hist2grad(hist, np.max(hist)*SIFT_ORI_PEAK_RATIO)
+        for _, ori in grad:
+            new_feat = feat.copy()
+            new_feat.ori = ori
+            feats.append(new_feat)
+    return feats
+
+
+def oriHist(img:np.ndarray, r, c, n, rad, sigma):
+    grad_mat = gradientMatrix(img, r, c, rad)
+    mag_wgt = generateGaussianKernel(ksize=2*rad+1, sigma=sigma)
+    hist = gradMat2Hist(grad_mat, mag_wgt, SIFT_ORI_HIST_BINS)
+    return hist
 
 
 
